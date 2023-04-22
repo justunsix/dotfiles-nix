7ec3a62ed5c86473576dde8b944028c149379c60
Justin <justin.tung@live.ca>
2023-04-22 01:41:18 -0400

    auto

--- home.nix
+++ home.nix
@@ -3,4 +3,45 @@
   home.homeDirectory = "/home/justin";
   home.stateVersion = "22.11";
   programs.home-manager.enable = true;
+
+  home.packages = [
+    pkgs.git
+    pkgs.bashInteractive # unsure why required?
+    pkgs.topgrade
+    pkgs.curl
+    pkgs.htop
+  ];
+
+  programs.bash = {    
+    enable = true;    
+    initExtra = builtins.readFile ./.bashrc;
+  };
+
+  programs.fzf = {   
+     enable = true;
+     enableBashIntegration = true;
+     enableFishIntegration = true;
+  };  
+
+  programs.zoxide = {    
+    enable = true;
+     enableBashIntegration = true;
+     enableFishIntegration = true;    
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  # Fish shell
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      "git s" = "git status";
+      "jgc" = "jt-gc.sh";
+      "jgs" = "jt-gs.sh";
+      "z" = "zoxide";
+    };
+  };
 }
