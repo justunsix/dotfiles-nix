82d485cac4b7e081665157b4cbee54a1181524c6
Justin Tung <justin.tung@live.ca>
2026-06-18 20:59:48 -0400

    chore(nix): formatting

--- flake.nix
+++ flake.nix
@@ -9,14 +9,15 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: let
-    arch = "x86_64-linux";
-  in {
-    defaultPackage.${arch} =
-      home-manager.defaultPackage.${arch};
+  outputs =
+    { nixpkgs, home-manager, ... }:
+    let
+      arch = "x86_64-linux";
+    in
+    {
+      defaultPackage.${arch} = home-manager.defaultPackage.${arch};
 
-    homeConfigurations.justin =
-      home-manager.lib.homeManagerConfiguration {
+      homeConfigurations.justin = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.${arch};
         modules = [ ./home.nix ];
       };
